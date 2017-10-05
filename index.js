ad57581d6839530c97bf5a9ac01f1a08704bb10f
Julia Lidstrom <juleslidstrom@gmail.com>
2017-10-05 00:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,19 @@ function multiply(a,b){
 function divide(a,b){
     number = a/b;
     return number;
-}
+};
 
 function inc(a){
   number = a+1;
   return number;
-}
+};
 
 function dec(a){
   number = a-1;
   return number;
-}
+};
+
+function makeInt(a,10){
+  number = parseInt(a,10);
+  return number;
+};
